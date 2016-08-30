0a8fde6e2f8a59c897b1a4685a86a9e99c6c9483
1223460708 <1223460708@qq.com>
2016-08-30 10:03:42 +0800

    Initial Commit

---
+++ CJBBeautifulPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJBBeautifulPhoto
+//
+//  Created by 炳神 on 16/8/30.
+//  Copyright © 2016年 as2482199. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJBBeautifulPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJBBeautifulPhoto
+//
+//  Created by 炳神 on 16/8/30.
+//  Copyright © 2016年 as2482199. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
